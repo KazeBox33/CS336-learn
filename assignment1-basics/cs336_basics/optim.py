46a14fcdfacefc7c3d11d7bab12348f133988deb
KazeBox33 <xdj483829269@163.com>
2026-06-09 14:46:06 +0800

    Implement AdamW optimizer

--- assignment1-basics/cs336_basics/optim.py
+++ assignment1-basics/cs336_basics/optim.py
@@ -29,10 +29,10 @@ class AdamW(torch.optim.Optimizer):
             eps=group["eps"]
             weight_decay=group["weight_decay"]
 
-            for p in group["params"]:
+            for p in group["params"]: # 表明的是每一组参数 
                 if p.grad is None:
                     continue
-                grad=p.grad
+                grad=p.grad    #反向传播后更新梯度
                  
                 state=self.state[p]   #optimizer基类维护的字典 用来给参数保存优化器自己的历史状态
 
@@ -43,7 +43,21 @@ class AdamW(torch.optim.Optimizer):
 
                 state["step"]+=1
 
-                exp_avg=state["exp_avg"]
-                exp_avg_sq=state["exp_avg_sq"]
+                exp_avg=state["exp_avg"]  #一阶动量， 用来维护历史平均方向
+                exp_avg_sq=state["exp_avg_sq"] #二阶动量， 用来维护历史平均大小
 
+                exp_avg.mul_(beta1).add_(grad,alpha=1-beta1) #更新一阶动量
+                exp_avg_sq.mul_(beta2).addcmul_(grad,grad,value=1-beta2) #更新二阶动量
                 
+                bias_correction1=1-beta1**state["step"] # 用于偏差修正的
+                bias_correction2=1-beta2**state["step"]
+
+                corrected_exp_avg=exp_avg/bias_correction1
+                corrected_exp_avg_sq=exp_avg_sq/bias_correction2
+                
+                update=corrected_exp_avg/(torch.sqrt(corrected_exp_avg_sq)+eps) #获取更新参数
+
+                p.data.mul_(1-lr*weight_decay)
+                p.data.add_(update,alpha=-lr)
+
+        return loss
